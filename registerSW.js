a7bf806447b15ebb0f881c53117ff237d7cb9866
TAHUDESU <tahudesu@gmail.com>
2026-02-25 19:54:47 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portofolio-Tbotics-Education/sw.js', { scope: '/Portofolio-Tbotics-Education/' })})}
